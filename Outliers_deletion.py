df3c46199c29cab2377c9126295b49cb5a884b73
cyber-Olga-2077 <krzyczkowskaolga@gmail.com>
2024-01-02 20:53:15 +0100

    Added correct outliers deletion

--- Outliers_deletion.py
+++ Outliers_deletion.py
@@ -31,30 +31,47 @@ dtypes={
     "DELAY_DUE_LATE_AIRCRAFT": "float64"
 }
 
+
 data_set = pd.read_csv('clean_data_set.csv', dtype=dtypes)
 searching_for_outliers = data_set.describe()
 print(tabulate(searching_for_outliers, headers='keys', tablefmt='psql'))
-searching_for_outliers_columns = data_set[["DEP_DELAY", "TAXI_OUT", "TAXI_IN", "ARR_DELAY"]]
+searching_for_outliers_regular_columns = data_set[["DEP_DELAY", "TAXI_OUT", "TAXI_IN", "ARR_DELAY"]]
+searching_for_outliers_delay_columns = data_set[["DELAY_DUE_CARRIER", "DELAY_DUE_WEATHER", "DELAY_DUE_NAS", "DELAY_DUE_SECURITY", "DELAY_DUE_LATE_AIRCRAFT"]]
 
 #we find outliers using IQR method and then we cap them using 3*std method
 
-def finding_and_capping_outliers(data_set, column_name):
-    q1 = data_set[column_name].quantile(0.25)
-    q3 = data_set[column_name].quantile(0.75)
+
+def finding_outliers_in_regular_columns(data):
+    q1 = data.quantile(0.1)
+    q3 = data.quantile(0.9)
     IQR = q3 - q1
-    outliers = data_set[((data_set[column_name]<(q1-1.5*IQR))|(data_set[column_name]>(q3+1.5*IQR)))]
-    upper_limit = data_set[column_name].mean() + 3*data_set[column_name].std()
-    lower_limit = data_set[column_name].mean() - 3*data_set[column_name].std()
-    data_set[column_name] = np.where(data_set[column_name] > upper_limit,
-         upper_limit,
-         np.where(
-                data_set[column_name] < lower_limit,
-                lower_limit,
-                data_set[column_name])
-         )
-    return data_set
+    outliers = data[((data < (q1-1.5*IQR)) | (data > (q3+1.5*IQR)))]
+    is_outlier = outliers.any(axis=1)
+    outliers_indices = set(is_outlier[is_outlier].index)
+    return outliers_indices
 
-for column in searching_for_outliers_columns:
-    data_set = finding_and_capping_outliers(data_set, column)
 
-print(data_set.describe())
+def finding_outliers_in_delay_columns(data):
+    q1 = data.quantile(0.1)
+    q3 = data.quantile(0.8)
+    IQR = q3 - q1
+    outliers = data[((data < (q1 - 1.5 * IQR)) | (data > (q3 + 1.5 * IQR)))]
+    is_outlier = outliers.any(axis=1)
+    outliers_indices = set(is_outlier[is_outlier].index)
+    return outliers_indices
+
+
+histogram = data_set.hist(bins=50, figsize=(12, 10))
+figure = histogram[0][0].get_figure()
+figure.savefig(f'Chart_before_dropping_outliers.pdf')
+
+outliers_indices = set()
+outliers_indices.update(finding_outliers_in_regular_columns(searching_for_outliers_regular_columns))
+outliers_indices.update(finding_outliers_in_delay_columns(searching_for_outliers_delay_columns))
+print(tabulate(data_set.describe(), headers='keys', tablefmt='psql'))
+data_set.drop(data_set.index[list(outliers_indices)], inplace=True)
+print(tabulate(data_set.describe(), headers='keys', tablefmt='psql'))
+
+histogram = data_set.hist(bins=50, figsize=(12, 10))
+figure = histogram[0][0].get_figure()
+figure.savefig(f'Chart_after_dropping_outliers.pdf')
