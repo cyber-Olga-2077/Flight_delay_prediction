9c70340ca30619cde3072451e1bf1201e6f730cf
Natalia Kuś <66189@student.ue.poznan.pl>
2024-01-07 19:41:28 +0100

    #dodanie kodu w krokach 4 i 5

--- Step4_transformations_and_pipeline.py
+++ Step4_transformations_and_pipeline.py
@@ -1,18 +1,20 @@
 import numpy as np
 import pandas as pd
-
+from sklearn.metrics.pairwise import rbf_kernel
+from sklearn.base import BaseEstimator
+from sklearn.base import TransformerMixin
 from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from collections import Counter
 
 data_set = pd.read_csv('flights_sample_3m.csv') #użyty ten plik zamiast whole_data_set,ponieważ były w nim potrzebne mi kolumny (więcej na messengerze ;), ale bedzie można zmienić na whole_data_set )
 data_set_num = data_set.select_dtypes(include=[np.number]) #wybranie wartości numerycznych
 data_set_cat = data_set.select_dtypes(exclude=[np.number]) #wybranie wartości inne niż numeryczne (kategoryczne)
 
-from sklearn.metrics.pairwise import rbf_kernel
-from sklearn.base import BaseEstimator
-from sklearn.base import TransformerMixin
-
 col_names = ["TAXI_IN", "TAXI_OUT", "AIR_TIME", "DISTANCE", "CRS_DEP_TIME"]
-r_ix, b_ix, p_ix, h_ix, j_ix = [data_set_num.columns.get_loc(c) for c in col_names] #nadawanie indeksów dla kolumn
+TI_ix, TO_ix, AT_ix, D_ix, CDT_ix = [data_set_num.columns.get_loc(c) for c in col_names] #nadawanie indeksów dla kolumn
 
 class CombinedAttrs(BaseEstimator, TransformerMixin):   #klasa transformacji
   def __init__(self, add_flight_speed=True):
@@ -22,22 +24,32 @@ class CombinedAttrs(BaseEstimator, TransformerMixin):   #klasa transformacji
     return self
 
   def transform(self, X):
-    flight_speed = X[:,h_ix] / X[:,p_ix]   #nowa miara
+    flight_speed = X[:,D_ix] / X[:,AT_ix]   #nowa miara
     #transformacje: log
     log_transformer = FunctionTransformer(np.log, inverse_func=np.exp)
-    log_dis = log_transformer.transform(X[:,h_ix])
-    log_taxi_in = log_transformer.transform(X[:,r_ix])
-    log_taxi_out = log_transformer.transform(X[:,b_ix])
+    log_dis = log_transformer.transform(X[:,D_ix])
+    log_taxi_in = log_transformer.transform(X[:,TI_ix])
+    log_taxi_out = log_transformer.transform(X[:,TO_ix])
     #transformacje: rbf
-    rbf_transformer = FunctionTransformer(rbf_kernel, kw_args=dict(Y=[[1800]], gamma=0.1)) # 1800 -> statystycznie najwięcej opóźnień ma miejsce o godzinie 18:00
-    delay_time_1800 = rbf_transformer.transform(X[:,j_ix].reshape(-1, 1))
+    dep_time = data_set["DEP_TIME"]
+    delays = data_set["DEP_DELAY"]
+
+    departure_and_delay = list(zip(dep_time, delays))
+
+    sum_delays_per_time = Counter()
+
+    for dep_time, delay in departure_and_delay:
+      sum_delays_per_time[dep_time] += delay
+
+    #Finding the hour with the highest sum of delays:
+    most_delays = max(sum_delays_per_time, key=sum_delays_per_time.get)
+
+    print(f"The most delays occurred at hour: {most_delays}")
+    rbf_transformer = FunctionTransformer(rbf_kernel, kw_args=dict(Y=[[most_delays]], gamma=0.1))
+    delay_time_1800 = rbf_transformer.transform(X[:,CDT_ix].reshape(-1, 1))
 
     return np.c_[X, flight_speed, log_dis, log_taxi_in, log_taxi_out, delay_time_1800]
 
-
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
 
 num_pipeline = Pipeline([
     ('imputer', SimpleImputer(strategy='median')),
@@ -49,3 +61,5 @@ data_set_transformed_num = num_pipeline.fit_transform(data_set_num)
 data_set_transformed_num_df = pd.DataFrame(data=data_set_transformed_num, columns=list(data_set_num.columns) + ['flight_speed', 'log_dis', 'log_taxi_in', 'log_taxi_out', 'delay_time_1800'])
 data_set_transformed = data_set_cat.join(data_set_transformed_num_df)
 #print(data_set_transformed)
+
+data_set_transformed.to_csv('data_set_transformed.csv', index=False, header=True)
