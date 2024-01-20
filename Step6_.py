c24006a59dd7e9edbda44ff85160ab78394c772f
Martyna Kolenda <89539@student.ue.poznan.pl>
2024-01-20 13:57:19 +0100

    step 6

---
+++ Step6_.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from math import sqrt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv('data_set_transformed.csv')
+data_num = data.select_dtypes(include=[np.number])
+X = data_num.drop('ARR_DELAY', axis=1)
+y = data_num['ARR_DELAY']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = RandomForestRegressor(n_estimators=100, max_depth=20, min_samples_leaf=50, min_samples_split=100)
+
+scores = cross_val_score(model, X, y, cv=5, scoring='neg_mean_squared_error')
+
+mean_rmse = np.mean(np.sqrt(-scores))
+print('RMSE: ', mean_rmse)
+
