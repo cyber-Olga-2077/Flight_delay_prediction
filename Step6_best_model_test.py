b3438845b7cb2f6cd0ec4b25d68fc2ed42507a75
Oliwia <68201@student.ue.poznan.pl>
2024-01-24 13:52:08 +0100

    testing example data with model

--- Step6_best_model_test.py
+++ Step6_best_model_test.py
@@ -4,12 +4,14 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score
 from scipy.stats import t
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from module import calculate_dist_between_airports
 
 
 train_set = pd.read_csv('train_data_set.csv')
 test_set = pd.read_csv('test_data_set.csv')
 train_set_num = train_set.select_dtypes(include=[np.number])
 test_set_num = test_set.select_dtypes(include=[np.number])
+
 X_train = train_set_num.drop('ARR_DELAY', axis=1)
 y_train = train_set_num['ARR_DELAY']
 X_test = test_set_num.drop('ARR_DELAY', axis=1)
@@ -20,7 +22,6 @@ forest.fit(X_train, y_train)
 y_pred_test = forest.predict(X_test)
 scores = cross_val_score(forest, X_test, y_test, cv=5, scoring='neg_mean_squared_error')
 
-#Trenowanie modelu
 mse_test = mean_squared_error(y_test, y_pred_test)
 rmse_test = np.sqrt(mse_test)
 mae_test = mean_absolute_error(y_test, y_pred_test)
@@ -49,3 +50,32 @@ print('Root Mean Squared Error:', rmse_test)
 print('Mean Absolute Error:', mae_test)
 print('R-squared:', r2_test)
 print("Based on these results, the model appears to be accurate and generalizes well to unseen data.")
+
+example_data = {
+    'FL_DATE': '2024-01-23',
+    'AIRLINE': 'Delta Air Lines Inc.',
+    'AIRLINE_CODE': 'DL',
+    'ORIGIN': 'JFK',
+    'ORIGIN_CITY': 'New York',
+    'DEST': 'LAX',
+    'DEST_CITY': 'Los Angeles',
+    'CRS_DEP_TIME': 1200,
+    'DISTANCE': calculate_dist_between_airports('JFK', 'LAX'),
+    'FL_MONTH': 1,
+}
+
+example_data = pd.DataFrame([example_data])
+example_data['FL_DATE'] = pd.to_datetime(example_data['FL_DATE'])
+
+#Dodanie brakujacych w przykladzie kolumn z modelu
+required_features = set(X_train.columns)
+for col in required_features:
+    if col not in example_data:
+        example_data[col] = np.nan
+
+#Zmiana kolejnosci kolumn na zgodna z modelem
+example_data = example_data[X_train.columns]
+
+#Predykcje modelu
+y_pred_example = forest.predict(example_data)
+print('Predicted Delay for the Example Data:', y_pred_example[0])
